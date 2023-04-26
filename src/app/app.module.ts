ab8c5143f4d41360158b15b3a51a785b7e4172f6
amorosAlvaro <alvaro.amoros.dev@gmail.com>
2023-04-26 12:11:42 +0200

    add: material, login, routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginRoutingModule } from './login/login-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, LoginRoutingModule, BrowserAnimationsModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/login/login-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    children: [
+      { path: '', component: SigninComponent },
+      { path: 'signup', component: SignupComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LoginRoutingModule {}
